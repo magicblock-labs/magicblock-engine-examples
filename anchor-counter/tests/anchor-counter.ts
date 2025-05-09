b7d516686c956daaa11776402cf263a16230bbcf
jonasXchen <jonas@metacamp.so>
2025-05-09 16:29:23 +0800

    upgraded packages and added increment-commit atomic ix and tests

--- anchor-counter/tests/anchor-counter.ts
+++ anchor-counter/tests/anchor-counter.ts
@@ -183,130 +183,130 @@ describe("anchor-counter", () => {
 });
 
 
-// describe("anchor-counter-atomic", () => {
+describe("anchor-counter-increment-commit-atomic", () => {
 
-//   // Configure the client to use the local cluster.
-//   const provider = anchor.AnchorProvider.env();
-//   anchor.setProvider(provider);
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-//   const providerEphemeralRollup = new anchor.AnchorProvider(
-//     new anchor.web3.Connection(
-//       process.env.PROVIDER_ENDPOINT || "https://devnet.magicblock.app/",
-//       {
-//         wsEndpoint: process.env.WS_ENDPOINT || "wss://devnet.magicblock.app/",
-//       }
-//     ),
-//     anchor.Wallet.local()
-//   );
+  const providerEphemeralRollup = new anchor.AnchorProvider(
+    new anchor.web3.Connection(
+      process.env.PROVIDER_ENDPOINT || "https://devnet.magicblock.app/",
+      {
+        wsEndpoint: process.env.WS_ENDPOINT || "wss://devnet.magicblock.app/",
+      }
+    ),
+    anchor.Wallet.local()
+  );
 
-//   before(async function () {
-//     const balance = await provider.connection.getBalance(anchor.Wallet.local().publicKey)
-//     console.log('Current balance is', balance / LAMPORTS_PER_SOL, ' SOL','\n')
-//   })
+  before(async function () {
+    const balance = await provider.connection.getBalance(anchor.Wallet.local().publicKey)
+    console.log('Current balance is', balance / LAMPORTS_PER_SOL, ' SOL','\n')
+  })
 
-//   const program = anchor.workspace.AnchorCounter as Program<AnchorCounter>;
-//   const [pda] = anchor.web3.PublicKey.findProgramAddressSync(
-//     [Buffer.from(SEED_TEST_PDA)],
-//     program.programId
-//   );
+  const program = anchor.workspace.AnchorCounter as Program<AnchorCounter>;
+  const [pda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(SEED_TEST_PDA)],
+    program.programId
+  );
 
-//   it("Initialize counter on Solana", async () => {
-//     const start = Date.now();
-//     const txHash = await program.methods
-//       .initialize()
-//       .accounts({
-//         // @ts-ignore
-//         counter: pda,
-//         user: provider.wallet.publicKey,
-//         systemProgram: anchor.web3.SystemProgram.programId,
-//       })
-//       .rpc({ skipPreflight: true });
-//     const duration = Date.now() - start;
-//     console.log(`${duration}ms (Base Layer) Initialize txHash: ${txHash}`);
+  it("Initialize counter on Solana", async () => {
+    const start = Date.now();
+    const txHash = await program.methods
+      .initialize()
+      .accounts({
+        // @ts-ignore
+        counter: pda,
+        user: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc({ skipPreflight: true });
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (Base Layer) Initialize txHash: ${txHash}`);
 
-//   });
+  });
 
-//   it("Delegate counter to ER", async () => {
-//     const start = Date.now();
-//     let tx = await program.methods
-//       .delegate()
-//       .accounts({
-//         payer: provider.wallet.publicKey,
-//         pda: pda,
-//       })
-//       .transaction();
-//     tx.feePayer = provider.wallet.publicKey;
-//     tx.recentBlockhash = (
-//       await provider.connection.getLatestBlockhash()
-//     ).blockhash;
-//     tx = await providerEphemeralRollup.wallet.signTransaction(tx);
-//     const txHash = await provider.sendAndConfirm(tx, [], {
-//       skipPreflight: true,
-//       commitment: "confirmed",
-//     });
-//     const duration = Date.now() - start;
-//     console.log(`${duration}ms (Base Layer) Delegate txHash: ${txHash}`);
-//   });
+  it("Delegate counter to ER", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .delegate()
+      .accounts({
+        payer: provider.wallet.publicKey,
+        pda: pda,
+      })
+      .transaction();
+    tx.feePayer = provider.wallet.publicKey;
+    tx.recentBlockhash = (
+      await provider.connection.getLatestBlockhash()
+    ).blockhash;
+    tx = await providerEphemeralRollup.wallet.signTransaction(tx);
+    const txHash = await provider.sendAndConfirm(tx, [], {
+      skipPreflight: true,
+      commitment: "confirmed",
+    });
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (Base Layer) Delegate txHash: ${txHash}`);
+  });
 
-//   it("Increase the delegate counter and commit through CPI", async () => {
-//     const start = Date.now();
-//     let tx = await program.methods
-//       .incrementAndCommit()
-//       .accounts({
-//         payer: providerEphemeralRollup.wallet.publicKey,
-//         // @ts-ignore
-//         counter: pda,
-//       })
-//       .transaction();
-//     tx.feePayer = providerEphemeralRollup.wallet.publicKey;
-//     tx.recentBlockhash = (
-//       await providerEphemeralRollup.connection.getLatestBlockhash()
-//     ).blockhash;
-//     tx = await providerEphemeralRollup.wallet.signTransaction(tx);
+  it("Increase the delegate counter and commit through CPI", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .incrementAndCommit()
+      .accounts({
+        payer: providerEphemeralRollup.wallet.publicKey,
+        // @ts-ignore
+        counter: pda,
+      })
+      .transaction();
+    tx.feePayer = providerEphemeralRollup.wallet.publicKey;
+    tx.recentBlockhash = (
+      await providerEphemeralRollup.connection.getLatestBlockhash()
+    ).blockhash;
+    tx = await providerEphemeralRollup.wallet.signTransaction(tx);
 
-//     const txHash = await providerEphemeralRollup.sendAndConfirm(tx, [], {
-//       skipPreflight: true,
-//     });
-//     const duration = Date.now() - start;
-//     console.log(`${duration}ms (ER) Increment And Commit txHash: ${txHash}`);
+    const txHash = await providerEphemeralRollup.sendAndConfirm(tx, [], {
+      skipPreflight: true,
+    });
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (ER) Increment And Commit txHash: ${txHash}`);
 
-//     // Await for the commitment on the base layer
-//     const txCommitSgn = await GetCommitmentSignature(
-//       txHash,
-//       providerEphemeralRollup.connection
-//     );
-//     console.log("Account commit signature:", txCommitSgn);
-//   });
+    // Await for the commitment on the base layer
+    const txCommitSgn = await GetCommitmentSignature(
+      txHash,
+      providerEphemeralRollup.connection
+    );
+    console.log("Account commit signature:", txCommitSgn);
+  });
 
-//   it("Increase the delegate counter and undelegate through CPI", async () => {
-//     const start = Date.now();
-//     let tx = await program.methods
-//       .incrementAndUndelegate()
-//       .accounts({
-//         payer: providerEphemeralRollup.wallet.publicKey,
-//         // @ts-ignore
-//         counter: pda,
-//       })
-//       .transaction();
-//     tx.feePayer = provider.wallet.publicKey;
-//     tx.recentBlockhash = (
-//       await providerEphemeralRollup.connection.getLatestBlockhash()
-//     ).blockhash;
-//     tx = await providerEphemeralRollup.wallet.signTransaction(tx);
+  it("Increase the delegate counter and undelegate through CPI", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .incrementAndUndelegate()
+      .accounts({
+        payer: providerEphemeralRollup.wallet.publicKey,
+        // @ts-ignore
+        counter: pda,
+      })
+      .transaction();
+    tx.feePayer = provider.wallet.publicKey;
+    tx.recentBlockhash = (
+      await providerEphemeralRollup.connection.getLatestBlockhash()
+    ).blockhash;
+    tx = await providerEphemeralRollup.wallet.signTransaction(tx);
 
-//     const txHash = await providerEphemeralRollup.sendAndConfirm(tx);
-//     const duration = Date.now() - start;
-//     console.log(`${duration}ms (ER) Increment and Undelegate txHash: ${txHash}`);
+    const txHash = await providerEphemeralRollup.sendAndConfirm(tx);
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (ER) Increment and Undelegate txHash: ${txHash}`);
 
-//     // Get the commitment signature on the base layer
-//     const comfirmCommitStart = Date.now();
-//     // Await for the commitment on the base layer
-//     const txCommitSgn = await GetCommitmentSignature(
-//         txHash,
-//         providerEphemeralRollup.connection
-//     );
-//     const commitDuration = Date.now() - comfirmCommitStart;
-//     console.log(`${commitDuration}ms (Base Layer) Undelegate txHash: ${txCommitSgn}`);
-//   });
+    // Get the undelegate signature on the base layer
+    const comfirmCommitStart = Date.now();
+    // Await for the commitment on the base layer
+    const txCommitSgn = await GetCommitmentSignature(
+        txHash,
+        providerEphemeralRollup.connection
+    );
+    const commitDuration = Date.now() - comfirmCommitStart;
+    console.log(`${commitDuration}ms (Base Layer) Undelegate txHash: ${txCommitSgn}`);
+  });
 
-// });
+});
