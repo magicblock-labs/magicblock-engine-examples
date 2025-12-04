53415c52314161b67d7354e63bdb1fe43f98dd89
fauxfire <andyweng97@gmail.com>
2025-12-04 12:29:18 +0400

    base

--- crank-counter/programs/crank-counter/src/lib.rs
+++ crank-counter/programs/crank-counter/src/lib.rs
@@ -1,16 +1,144 @@
 use anchor_lang::prelude::*;
+use ephemeral_rollups_sdk::anchor::{commit, delegate, ephemeral};
+use ephemeral_rollups_sdk::cpi::DelegateConfig;
+use ephemeral_rollups_sdk::ephem::{commit_accounts, commit_and_undelegate_accounts};
+
 
 declare_id!("5LomaZ9w94qfwvdDa1QhhCe41HYqrQTkdYfP39pX6LqH");
 
+
+pub const COUNTER_SEED: &[u8] = b"counter";
+
+#[ephemeral]
 #[program]
-pub mod crank_counter {
+pub mod anchor_counter {
     use super::*;
 
+    /// Initialize the counter.
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("PDA {} count: {}", counter.key(), counter.count);
+        Ok(())
+    }
+
+    /// Increment the counter.
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        if counter.count > 1000 {
+            counter.count = 0;
+        }
+        msg!("PDA {} count: {}", counter.key(), counter.count);
+        Ok(())
+    }
+
+    /// Delegate the account to the delegation program
+    /// Set specific validator based on ER, see https://docs.magicblock.gg/pages/get-started/how-integrate-your-program/local-setup
+    pub fn delegate(ctx: Context<DelegateInput>) -> Result<()> {
+        ctx.accounts.delegate_pda(
+            &ctx.accounts.payer,
+            &[COUNTER_SEED],
+            DelegateConfig {
+                // Optionally set a specific validator from the first remaining account
+                validator: ctx.remaining_accounts.first().map(|acc| acc.key()),
+                ..Default::default()
+            },
+        )?;
+        Ok(())
+    }
+
+    /// Manual commit the account in the ER.
+    pub fn commit(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        commit_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
+
+    /// Undelegate the account from the delegation program
+    pub fn undelegate(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        commit_and_undelegate_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
+
+    /// Increment the counter + manual commit the account in the ER.
+    pub fn increment_and_commit(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        msg!("PDA {} count: {}", counter.key(), counter.count);
+        counter.exit(&crate::ID)?;
+        commit_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
+        Ok(())
+    }
+
+    /// Increment the counter + manual commit the account in the ER.
+    pub fn increment_and_undelegate(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        msg!("PDA {} count: {}", counter.key(), counter.count);
+        // Serialize the Anchor counter account, commit and undelegate
+        counter.exit(&crate::ID)?;
+        commit_and_undelegate_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program,
+        )?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init_if_needed, payer = user, space = 8 + 8, seeds = [COUNTER_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+/// Add delegate function to the context
+#[delegate]
+#[derive(Accounts)]
+pub struct DelegateInput<'info> {
+    pub payer: Signer<'info>,
+    /// CHECK The pda to delegate
+    #[account(mut, del)]
+    pub pda: AccountInfo<'info>,
+}
+
+/// Account for the increment instruction.
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, seeds = [COUNTER_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+}
+
+/// Account for the increment instruction + manual commit.
+#[commit]
+#[derive(Accounts)]
+pub struct IncrementAndCommit<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(mut, seeds = [COUNTER_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
--- crank-counter/tests/crank-counter.ts
+++ crank-counter/tests/crank-counter.ts
@@ -1,16 +1,197 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CrankCounter } from "../target/types/crank_counter";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { GetCommitmentSignature } from "@magicblock-labs/ephemeral-rollups-sdk";
+
+const COUNTER_SEED = "counter"; 
 
 describe("crank-counter", () => {
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.crankCounter as Program<CrankCounter>;
+  const providerEphemeralRollup = new anchor.AnchorProvider(
+    new anchor.web3.Connection(
+      process.env.EPHEMERAL_PROVIDER_ENDPOINT ||
+        "https://devnet-as.magicblock.app/",
+      {
+        wsEndpoint:
+          process.env.EPHEMERAL_WS_ENDPOINT || "wss://devnet-as.magicblock.app/",
+      },
+    ),
+    anchor.Wallet.local(),
+  );
+  console.log("Base Layer Connection: ", provider.connection.rpcEndpoint);
+  console.log(
+    "Ephemeral Rollup Connection: ",
+    providerEphemeralRollup.connection.rpcEndpoint,
+  );
+  console.log(`Current SOL Public Key: ${anchor.Wallet.local().publicKey}`);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  before(async function () {
+    const balance = await provider.connection.getBalance(
+      anchor.Wallet.local().publicKey,
+    );
+    console.log("Current balance is", balance / LAMPORTS_PER_SOL, " SOL", "\n");
+  });
+
+  const program = anchor.workspace.AnchorCounter as Program<AnchorCounter>;
+  const [counterPDA] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(COUNTER_SEED)],
+    program.programId,
+  );
+
+  console.log("Program ID: ", program.programId.toString());
+  console.log("Counter PDA: ", counterPDA.toString());
+
+  it("Initialize counter on Solana", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .initialize()
+      .accounts({
+        user: provider.wallet.publicKey,
+      })
+      .transaction();
+
+    const txHash = await provider.sendAndConfirm(tx, [provider.wallet.payer], {
+      skipPreflight: true,
+      commitment: "confirmed",
+    });
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (Base Layer) Initialize txHash: ${txHash}`);
+  });
+
+  it("Increase counter on Solana", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .increment()
+      .accounts({
+        counter: counterPDA,
+      })
+      .transaction();
+    const txHash = await provider.sendAndConfirm(tx, [provider.wallet.payer], {
+      skipPreflight: true,
+      commitment: "confirmed",
+    });
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (Base Layer) Increment txHash: ${txHash}`);
+  });
+
+  it("Delegate counter to ER", async () => {
+    const start = Date.now();
+    // Add local validator identity to the remaining accounts if running on localnet
+    const remainingAccounts =
+      providerEphemeralRollup.connection.rpcEndpoint.includes("localhost") ||
+      providerEphemeralRollup.connection.rpcEndpoint.includes("127.0.0.1")
+        ? [
+            {
+              pubkey: new web3.PublicKey("mAGicPQYBMvcYveUZA5F5UNNwyHvfYh5xkLS2Fr1mev"),
+              isSigner: false,
+              isWritable: false,
+            },
+          ]
+        : [];
+    let tx = await program.methods
+      .delegate()
+      .accounts({
+        payer: provider.wallet.publicKey,
+        pda: counterPDA,
+      })
+      .remainingAccounts(remainingAccounts)
+      .transaction();
+    const txHash = await provider.sendAndConfirm(tx, [provider.wallet.payer], {
+      skipPreflight: true,
+      commitment: "confirmed",
+    });
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (Base Layer) Delegate txHash: ${txHash}`);
+  });
+
+  it("Increase counter on ER", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .increment()
+      .accounts({
+        counter: counterPDA,
+      })
+      .transaction();
+    tx.feePayer = providerEphemeralRollup.wallet.publicKey;
+    tx.recentBlockhash = (
+      await providerEphemeralRollup.connection.getLatestBlockhash()
+    ).blockhash;
+    tx = await providerEphemeralRollup.wallet.signTransaction(tx);
+    const txHash = await providerEphemeralRollup.sendAndConfirm(tx);
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (ER) Increment txHash: ${txHash}`);
+  });
+
+  it("Commit counter state on ER to Solana", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .commit()
+      .accounts({
+        payer: providerEphemeralRollup.wallet.publicKey,
+      })
+      .transaction();
+    tx.feePayer = providerEphemeralRollup.wallet.publicKey;
+    tx.recentBlockhash = (
+      await providerEphemeralRollup.connection.getLatestBlockhash()
+    ).blockhash;
+    tx = await providerEphemeralRollup.wallet.signTransaction(tx);
+
+    const txHash = await providerEphemeralRollup.sendAndConfirm(tx, [], {
+      skipPreflight: true,
+    });
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (ER) Commit txHash: ${txHash}`);
+
+    // Get the commitment signature on the base layer
+    const comfirmCommitStart = Date.now();
+    // Await for the commitment on the base layer
+    const txCommitSgn = await GetCommitmentSignature(
+      txHash,
+      providerEphemeralRollup.connection,
+    );
+    const commitDuration = Date.now() - comfirmCommitStart;
+    console.log(
+      `${commitDuration}ms (Base Layer) Commit txHash: ${txCommitSgn}`,
+    );
+  });
+
+  it("Increase counter on ER and commit", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .incrementAndCommit()
+      .accounts({})
+      .transaction();
+    tx.feePayer = providerEphemeralRollup.wallet.publicKey;
+    tx.recentBlockhash = (
+      await providerEphemeralRollup.connection.getLatestBlockhash()
+    ).blockhash;
+    tx = await providerEphemeralRollup.wallet.signTransaction(tx);
+    const txHash = await providerEphemeralRollup.sendAndConfirm(tx);
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (ER) Increment and Commit txHash: ${txHash}`);
+  });
+
+  it("Increment and undelegate counter on ER to Solana", async () => {
+    const start = Date.now();
+    let tx = await program.methods
+      .incrementAndUndelegate()
+      .accounts({
+        payer: providerEphemeralRollup.wallet.publicKey,
+      })
+      .transaction();
+    tx.feePayer = provider.wallet.publicKey;
+    tx.recentBlockhash = (
+      await providerEphemeralRollup.connection.getLatestBlockhash()
+    ).blockhash;
+    tx = await providerEphemeralRollup.wallet.signTransaction(tx);
+
+    const txHash = await providerEphemeralRollup.sendAndConfirm(tx);
+    const duration = Date.now() - start;
+    console.log(`${duration}ms (ER) Increment and Undelegate txHash: ${txHash}`);
   });
 });
